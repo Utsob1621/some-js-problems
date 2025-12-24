f04489eeb539f22a23cafc07db5066ba3ed5b396
Utsob1621 <utsobsaha9072@gmail.com>
2025-12-24 12:10:39 +0600

    problem 3 stucture is added now

---
+++ problem-3.js
@@ -0,0 +1,15 @@
+/*Problem-03 : Who will Win  ( 🥭Mango /🍌 Banana )*/ 
+
+function electionResult(votes){
+
+}
+
+
+const result = electionResult(["mango", "banana", "Na-Vote", "mango", "banana", "banana", "Na-Vote", "banana", "mango", "Na-Vote", "Na-Vote", "mango", "banana", "tomato"]);
+console.log(result);
+
+
+
+
+
+
