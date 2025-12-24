89f70103d7674134802026fde4ee500a6b02e02c
Utsob1621 <utsobsaha9072@gmail.com>
2025-12-24 12:17:48 +0600

    vote counting is complete

--- problem-3.js
+++ problem-3.js
@@ -3,6 +3,19 @@
 function electionResult(votes){
   if(!Array.isArray(votes)){
     return "Invalid"
+
+
+    let mangoCount = 0;
+    let bananaCount = 0;
+    for(let vote of votes){
+      if(vote === "mango"){
+        mangoCount++;
+      }
+
+      else if(vote === "banana"){
+        bananaCount++;
+      }
+    }
   }
 
 }
