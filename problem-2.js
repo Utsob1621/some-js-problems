623c1ddb57ae3acadd2cb63872eeaa51e2e69047
Utsob1621 <utsobsaha9072@gmail.com>
2025-12-24 12:34:43 +0600

    added some sample input and output

--- problem-2.js
+++ problem-2.js
@@ -30,36 +30,3 @@ function validEmail(email){
 const result = validEmail("utsobsaha9074@gmail.com");
 console.log(result);
 
-
-\// SAMPLE INPUT
-// ["mango", "banana", "mango", "banana", "mango"]
-// SAMPLE OUTPUT
-// "Mango"
-// SAMPLE INPUT
-// []
-// SAMPLE OUTPUT
-// "Draw"
-// SAMPLE INPUT
-// ["mango", "banana", "jaker party" , "no"]
-// SAMPLE OUTPUT
-// "Draw"
-// SAMPLE INPUT
-// ["mango"]
-// SAMPLE OUTPUT
-// "Mango"
-// SAMPLE INPUT
-// ["banana", "banana", "age e valo chilam" , "no"]
-// SAMPLE OUTPUT
-// "Banana"
-// SAMPLE INPUT
-// {result: "mango , banana ,  mango"}
-// SAMPLE OUTPUT
-// "Invalid"
-// SAMPLE INPUT
-// "mango , banana"
-// SAMPLE OUTPUT
-// "Invalid"
-// SAMPLE INPUT
-// ["mango", "BananA", "na vote", "na vote"]
-// SAMPLE OUTPUT
-// "Mango"
--- problem-3.js
+++ problem-3.js
@@ -35,6 +35,41 @@ console.log(result);
 
 
 
+// Some sample input and sample output
+
+// SAMPLE INPUT
+// ["mango", "banana", "mango", "banana", "mango"]
+// SAMPLE OUTPUT
+// "Mango"
+// SAMPLE INPUT
+// []
+// SAMPLE OUTPUT
+// "Draw"
+// SAMPLE INPUT
+// ["mango", "banana", "jaker party" , "no"]
+// SAMPLE OUTPUT
+// "Draw"
+// SAMPLE INPUT
+// ["mango"]
+// SAMPLE OUTPUT
+// "Mango"
+// SAMPLE INPUT
+// ["banana", "banana", "age e valo chilam" , "no"]
+// SAMPLE OUTPUT
+// "Banana"
+// SAMPLE INPUT
+// {result: "mango , banana ,  mango"}
+// SAMPLE OUTPUT
+// "Invalid"
+// SAMPLE INPUT
+// "mango , banana"
+// SAMPLE OUTPUT
+// "Invalid"
+// SAMPLE INPUT
+// ["mango", "BananA", "na vote", "na vote"]
+// SAMPLE OUTPUT
+// "Mango"
+
 
 
 
