452031570e8c60989144d3c5b8971994ca00bddd
Utsob1621 <utsobsaha9072@gmail.com>
2025-12-24 22:51:43 +0600

    chekced f1 and f3 is an object or not

--- problem-4.js
+++ problem-4.js
@@ -2,12 +2,22 @@
 
 friend1Details = {name : "Bidhan Saha", roll : 1, bestFriend : 2}
 
-friend2Details = {name : "Borson Saha", roll : 2, bestFriend : 1}
+friend2Details = {name : "Borson Saha", roll : 2, bestFriend : 11}
 
 
 function isBestFriend(f1, f2){
+
+  if(typeof f1 !== "object" || typeof f2 !== "object" || f1 === null || f2 === null){
+    return "Invalid";
+  }
+
+
+  
     if(f1.roll === f2.bestFriend && f2.bestFriend === f1.roll){
-      return true
+      return true;
+    }
+    else{
+      return false;
     }
 }
 
