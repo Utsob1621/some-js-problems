f8d93280538627d4c6582019e3028ece0bc5cc18
Utsob1621 <utsobsaha9072@gmail.com>
2025-12-25 01:32:22 +0600

    problem 5 is done

--- problem-5.js
+++ problem-5.js
@@ -5,11 +5,22 @@ function calculateWatchTime(times){
   if(!Array.isArray(times)){
     return "Invalid";
   }
+  
 
-  if(times !== "number"){
+  let allNumbers = times.every(el => typeof el === "number" && !Number.isNaN(el));
+  if(!allNumbers){
     return "Invalid"
   }
+
+  let totalSeconds = times.reduce((sum, el) => sum + el, 0);
+
+  let hour = Math.floor(totalSeconds / 3600);
+  let minute = Math.floor((totalSeconds % 3600) / 60);
+  let second = totalSeconds % 60;
+
+  return {hour, minute, second};
 }
+
 
 const result = calculateWatchTime([100, 99, 119, 300]);
 console.log(result);
